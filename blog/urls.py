9e5c135dc83d6ddeac85adef9ec3f8f065def287
Piotr Baran <pb_piotrbaran@wp.pl>
2023-10-26 13:12:12 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), #<int:pk> - ta część jest trudniejsza. Oznacza ona, że Django spodziewa się liczby całkowitej i przekaże jej wartość do widoku jako zmienną pk
+    path('PB/', views.PB, name='PB'),
 ]
