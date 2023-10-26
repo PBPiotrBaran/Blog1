7a7b72f7b428a78bd69b59414e25642f4b75f79e
Piotr Baran <pb_piotrbaran@wp.pl>
2023-10-26 15:15:32 +0200

    Po kursie

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'), #<int:pk> - ta część jest trudniejsza. Oznacza ona, że Django spodziewa się liczby całkowitej i przekaże jej wartość do widoku jako zmienną pk
+
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
     path('PB/', views.PB, name='PB'),
 ]
