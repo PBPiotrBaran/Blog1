721cf208d5c6d7e038806d813496e7cfb2782888
Piotr Baran <pb_piotrbaran@wp.pl>
2023-10-26 09:50:23 +0200

    Zmieniony kod HTML na stronie.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,35 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Post
 
-admin.site.register(Post)
+#admin.site.register(Post)
+
+class Voteinline(admin.TabularInline):
+   # model = Post # muszę zefiniować nowy model który załaduje dane w tabeli liniowo dane do modelu post
+    pass
+
+
+#dekorowanie, nadpisywanie zdefiniowanych klas systemu
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author','title','show_url']
+    list_filter = ['author']
+    #list_display_links = ['text']
+    search_fields = ['title']
+   # inlines = Voteinline
+
+
+    def show_url(self,obj):
+        if len(obj.text) is not None:
+            return format_html(f'<a href="{obj.text}" target = blanc > {obj.text}</a>')
+        else:
+            return ''
+
+    show_url.short_description = 'URL'
+
+
+#@admin.register(Vote)
+#class VoteAdmin(admin.ModelAdmin):
+ #   pass
+
+
